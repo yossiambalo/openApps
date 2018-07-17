abf40a2f91c9a61604c672f29252ad1e3276a070
yossi.ambalo <yossi.ambalo@odysii.com>
2018-07-17 16:58:38 +0300

    Summery class

---
+++ src/main/java/com/odysii/selenium/page/myApps/summery/Summery.java
@@ -0,0 +1,10 @@
+package com.odysii.selenium.page.myApps.summery;
+
+import com.odysii.selenium.page.util.PageObject;
+import org.openqa.selenium.WebDriver;
+
+public class Summery extends PageObject {
+    public Summery(WebDriver driver) {
+        super(driver);
+    }
+}
