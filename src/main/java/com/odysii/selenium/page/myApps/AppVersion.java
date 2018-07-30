e209d140fdb1828c4370397936eb3c3959e01cdb
yossiambalo <Panda250>
2018-07-30 17:35:03 +0300

    AppVeriosTest

--- src/main/java/com/odysii/selenium/page/myApps/AppVersion.java
+++ src/main/java/com/odysii/selenium/page/myApps/AppVersion.java
@@ -11,8 +11,8 @@ public class AppVersion extends PageObject {
     WebElement NewVersion;
     @FindBy(id = "appVersionsUploadCode")
     WebElement SelectUpload;
-    @FindBy(id = "new-version-number")
-    WebElement NewVeriosnNumber;
+    @FindBy(id = "newVersionNumber")
+    WebElement NewVersionNumber;
     @FindBy(id = "new-version-text")
     WebElement NewVersionText;
     @FindBy(id = "finish-button")
@@ -23,25 +23,37 @@ public class AppVersion extends PageObject {
         super(driver);
 
     }
-    public void editAppVersion(FieldType type, String newValue){
-    switch (type){
+    public void editAppVersion(FieldType newVersionText, String s, FieldType type, String newValue, FieldType selectUpload) {
+        switch (type) {
 
-        case NEW_VERSION_NUMBER:
-            NewVersion.click();
-            this.NewVeriosnNumber.clear();
-            this.NewVeriosnNumber.sendKeys(newValue);
-            break;
-        case NEW_VERSION_TEXT:
-            this.NewVersionText.clear();
-            this.NewVersionText.sendKeys(newValue);
-            break;
-        case SELECT_UPLOAD:
-            this.SelectUpload.sendKeys(newValue);
-            break;
+            case NEW_VERSION_NUMBER:
+                NewVersion.click();
+                this.NewVersionNumber.clear();
+                this.NewVersionNumber.sendKeys(newValue);
+                break;
+            case NEW_VERSION_TEXT:
+                NewVersion.click();
+                this.NewVersionText.clear();
+                this.NewVersionText.sendKeys(newValue);
+                break;
+            case SELECT_UPLOAD:
+                NewVersion.click();
+                this.SelectUpload.sendKeys(newValue);
+                break;
 
             default:
 
+        }
+    }
+        public void editAppVersion(String appVersion, String newVersionText, String selectUpload){
+            NewVersion.click();
+            this.NewVersionNumber.clear();
+            this.NewVersionNumber.sendKeys(appVersion);
+            this.NewVersionText.clear();
+            this.NewVersionText.sendKeys(newVersionText);
+            this.SelectUpload.sendKeys(selectUpload);
 
         }
     }
-}
+
+
