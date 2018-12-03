5483a8dc0628e1bf334f38a8df735f51f65b8711
yossi.ambalo <yossi.ambalo@odysii.com>
2018-12-03 16:39:38 +0200

    Yossi: More funcionality for admin user

---
+++ src/main/java/com/odysii/selenium/page/openApps/admin/RetailersPage.java
@@ -0,0 +1,10 @@
+package com.odysii.selenium.page.openApps.admin;
+
+import com.odysii.selenium.page.util.PageObject;
+import org.openqa.selenium.WebDriver;
+
+public class RetailersPage extends PageObject {
+    public RetailersPage(WebDriver driver) {
+        super(driver);
+    }
+}
