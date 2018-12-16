e7db5dedcf94a0dcb0941a4b727ed4dca33cc0fc
yossi.ambalo <yossi.ambalo@odysii.com>
2018-12-16 17:04:15 +0200

    Admin test added!

--- src/main/java/com/odysii/selenium/page/openApps/admin/RetailersPage.java
+++ src/main/java/com/odysii/selenium/page/openApps/admin/RetailersPage.java
@@ -1,5 +1,7 @@
 package com.odysii.selenium.page.openApps.admin;
 
+import com.odysii.selenium.page.openApps.admin.helper.EnviromentType;
+import com.odysii.selenium.page.util.FileHandler;
 import com.odysii.selenium.page.util.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,21 +14,89 @@ public class RetailersPage extends PageObject {
 
     @FindBy(className = "admin-retailer-row")
     private List<WebElement> retailerDivs;
+    private final String SUCCESS_MESAGE = "Revoke";
     public RetailersPage(WebDriver driver) {
         super(driver);
     }
     public void getEditPage(String retailer){
         boolean flag = false;
         isElementPresent(retailerDivs.get(0));
-        for (WebElement e : retailerDivs){
-            if(e.findElement(By.className("child_center")).getText().toLowerCase().equals(retailer.trim().toLowerCase())){
-                e.findElement(By.className("col-auto")).click();
+        for (int i = 0; i < retailerDivs.size()-1; i++){
+            if(retailerDivs.get(i).findElement(By.className("child_center")).getText().toLowerCase().equals(retailer.replaceAll("[\\s|\\u00A0]+", "").toLowerCase())){
+                retailerDivs.get(i).findElement(By.className("block-item-menu-icon")).click();
                 flag = true;
+                webDriver.findElement(By.id("editRetailer"+(i+1))).click();
                 break;
             }
         }
         if (!flag){
             throw new WebDriverException("Retailer with name: "+retailer+" not found, please make sure name is exist!");
         }
+    }
+    public boolean generate(EnviromentType enviromentType){
+        int counter = 0;
+        while (!isElementPresent(By.id("GenerateProdEnvKeys")) && counter < 5){
+            wait(WAIT);
+            counter ++;
+        }
+        WebElement generateProdKeysElm = webDriver.findElement(By.id("GenerateProdEnvKeys"));
+        boolean res = false;
+        if (enviromentType.equals(EnviromentType.PROD)){
+            generateProdKeysElm.click();
+            counter = 0;
+            while (!isElementPresent(By.id("RevokeProdEnvKeys")) && counter < 5){
+                wait(WAIT);
+                counter ++;
+            }
+            res =  webDriver.findElement(By.id("RevokeProdEnvKeys")).getText().contains(SUCCESS_MESAGE);
+        }else {
+
+        }
+        return res;
+    }
+    public void downloadKey(EnviromentType enviromentType){
+        int counter = 0;
+        while (!isElementPresent(By.id("DownloadProdEnvKey")) && counter < 5){
+            wait(WAIT);
+            counter ++;
+        }
+        WebElement downloadProdKeysElm = webDriver.findElement(By.id("DownloadProdEnvKey"));
+        if (enviromentType.equals(EnviromentType.PROD)){
+            downloadProdKeysElm.click();
+        }else {
+
+        }
+    }
+    public boolean uploadOmnia(EnviromentType enviromentType,String filePath){
+        boolean res = false;
+        int counter = 0;
+        while (!isElementPresent(By.id("uploadProdEnvOmniaKeyInput")) && counter < 5){
+            wait(WAIT);
+            counter ++;
+        }
+        WebElement uploadProdKeysElm = webDriver.findElement(By.id("uploadProdEnvOmniaKeyInput"));
+        if (enviromentType.equals(EnviromentType.PROD)){
+            uploadProdKeysElm.sendKeys(filePath);
+            res = webDriver.findElement(By.className("footer-message")).getText().contains("Uploaded key successfully");
+        }else {
+
+        }
+        return res;
+    }
+    public boolean uploadGSOM(EnviromentType enviromentType,String filePath){
+        boolean res = false;
+        int counter = 0;
+        while (!isElementPresent(By.id("uploadProdEnvGsomKeyInput")) && counter < 5){
+            wait(WAIT);
+            counter ++;
+        }
+        WebElement uploadProdKeysElm = webDriver.findElement(By.id("uploadProdEnvGsomKeyInput"));
+        if (enviromentType.equals(EnviromentType.PROD)){
+            uploadProdKeysElm.sendKeys(filePath);
+            res = webDriver.findElement(By.className("footer-message")).getText().contains("Uploaded key successfully");
+        }else {
+
+        }
+        return res;
     }
 }
--- src/test/java/com/odysii/functional/AdminTest.java
+++ src/test/java/com/odysii/functional/AdminTest.java
@@ -4,10 +4,14 @@ import com.odysii.TestBase;
 import com.odysii.selenium.page.openApps.User;
 import com.odysii.selenium.page.openApps.UserType;
 import com.odysii.selenium.page.openApps.admin.AdminPage;
+import com.odysii.selenium.page.openApps.admin.RetailersPage;
 import com.odysii.selenium.page.openApps.admin.UsersPage;
+import com.odysii.selenium.page.openApps.admin.helper.EnviromentType;
 import com.odysii.selenium.page.openApps.admin.helper.RoleType;
 import com.odysii.selenium.page.openApps.helper.appDetails.RetailerType;
+import com.odysii.selenium.page.util.FileHandler;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -16,11 +20,14 @@ import java.util.List;
 
 public class AdminTest extends TestBase {
     AdminPage adminPage;
+    RetailersPage retailersPage;
+    FileHandler fileHandler = null;
+    final String GENERATED_KEY_FILE_PATH = System.getProperty("user.home")+"\\Downloads\\cert_pub_retailer_id_1.txt";
     @BeforeClass
     public void init(){
         category = "Admin Console";
     }
-    @Test(priority = 1)
+    //@Test(priority = 1)
     public void _001_valid_edit_users_in_admin_console(){
         user = new User(driver);
         adminPage = (AdminPage) user.login(ADMIN_USER_NAME,ADMIN_USER_PASS, UserType.ADMIN);
@@ -36,6 +43,51 @@ public class AdminTest extends TestBase {
     public void _002_valid_generate_key(){
         user = new User(driver);
         adminPage = (AdminPage) user.login(ADMIN_USER_NAME,ADMIN_USER_PASS, UserType.ADMIN);
-        UsersPage usersPage = adminPage.getUsersPage();
+        retailersPage = adminPage.getRetailersPage();
+        retailersPage.getEditPage(RetailerType.EXXON_MOBIL);
+        Assert.assertTrue(retailersPage.generate(EnviromentType.PROD));
+    }
+    @Test(priority = 3)
+    public void _003_valid_generate_key(){
+        if(!driver.getCurrentUrl().contains("manage-keys")){
+            user = new User(driver);
+            adminPage = (AdminPage) user.login(ADMIN_USER_NAME,ADMIN_USER_PASS, UserType.ADMIN);
+            retailersPage = adminPage.getRetailersPage();
+            retailersPage.getEditPage(RetailerType.EXXON_MOBIL);
+        }
+        retailersPage.downloadKey(EnviromentType.PROD);
+        int counter = 0;
+        fileHandler = new FileHandler();
+        while (!fileHandler.isFileExist(GENERATED_KEY_FILE_PATH) && counter < 5){
+            wait(WAIT);
+            counter ++;
+        }
+        Assert.assertTrue(fileHandler.isFileExist(GENERATED_KEY_FILE_PATH));
+    }
+    @Test(priority = 4)
+    public void _004_valid_upload_key_omnia(){
+        if(!driver.getCurrentUrl().contains("manage-keys")){
+            user = new User(driver);
+            adminPage = (AdminPage) user.login(ADMIN_USER_NAME,ADMIN_USER_PASS, UserType.ADMIN);
+            retailersPage = adminPage.getRetailersPage();
+            retailersPage.getEditPage(RetailerType.EXXON_MOBIL);
+        }
+        Assert.assertTrue(retailersPage.uploadOmnia(EnviromentType.PROD,GENERATED_KEY_FILE_PATH));
+    }
+    @Test(priority = 5)
+    public void _005_valid_upload_key_gesom(){
+        if(!driver.getCurrentUrl().contains("manage-keys")){
+            user = new User(driver);
+            adminPage = (AdminPage) user.login(ADMIN_USER_NAME,ADMIN_USER_PASS, UserType.ADMIN);
+            retailersPage = adminPage.getRetailersPage();
+            retailersPage.getEditPage(RetailerType.EXXON_MOBIL);
+        }
+        Assert.assertTrue(retailersPage.uploadGSOM(EnviromentType.PROD,GENERATED_KEY_FILE_PATH));
+    }
+    @AfterClass
+    public void clean(){
+        if (fileHandler != null){
+            fileHandler.deleteFile(GENERATED_KEY_FILE_PATH);
+        }
     }
 }
