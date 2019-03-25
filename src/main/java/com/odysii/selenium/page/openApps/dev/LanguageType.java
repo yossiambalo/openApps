bbfa8e0d09094282fb43fad048f618aa86b0340f
Roi <roi.avital@odysii.com>
2019-03-25 14:44:56 +0200

    Commit Roi 25.3.19

--- src/main/java/com/odysii/selenium/page/openApps/dev/LanguageType.java
+++ src/main/java/com/odysii/selenium/page/openApps/dev/LanguageType.java
@@ -1,7 +1,9 @@
 package com.odysii.selenium.page.openApps.dev;
 
 public class LanguageType {
+
     public static final String ENGLISH = "English";
     public static final String FRENCH = "French";
     public static final String SPANISH = "Spanish";
+
 }
