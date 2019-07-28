76e4cce4e4a2c313f44dd3896bb769dab16e2171
yossi.ambalo <yossi.ambalo@odysii.com>
2019-07-28 15:24:30 +0300

    AddUser deleteUser fixed!

--- src/test/java/com/odysii/functional/AddUserTest.java
+++ src/test/java/com/odysii/functional/AddUserTest.java
@@ -36,7 +36,7 @@ public class AddUserTest extends TestBase {
 
     }
 
-    //@Test
+    @Test
     public void _002_cant_add_duplicate_email_address(){
         UsersPage usersPage = adminPage.getUsersPage();
         usersPage.getAddNewUserPage();
@@ -49,7 +49,7 @@ public class AddUserTest extends TestBase {
 
     }
 
-    //@Test
+    @Test
     public void _003_mandatory_fields_add_new_user_page_empty_name(){
         UsersPage usersPage = adminPage.getUsersPage();
         usersPage.getAddNewUserPage();
@@ -62,7 +62,7 @@ public class AddUserTest extends TestBase {
 
     }
 
-    //@Test
+    @Test
     public void _004_mandatory_fields_add_new_user_page_empty_email_address(){
         UsersPage usersPage = adminPage.getUsersPage();
         usersPage.getAddNewUserPage();
@@ -75,7 +75,7 @@ public class AddUserTest extends TestBase {
 
     }
 
-  //@Test
+  @Test
     public void _005_mandatory_fields_add_new_user_page_empty_role(){
         UsersPage usersPage = adminPage.getUsersPage();
         usersPage.getAddNewUserPage();
@@ -88,7 +88,7 @@ public class AddUserTest extends TestBase {
 
     }
 
-    //@Test
+    @Test
     public void _006_mandatory_fields_add_new_user_page_empty_delegations(){
         UsersPage usersPage = adminPage.getUsersPage();
         usersPage.getAddNewUserPage();
@@ -101,7 +101,7 @@ public class AddUserTest extends TestBase {
 
     }
 
-   //@Test
+   @Test
     public void _007_create_new_user_with_org_override_CONFIRM_message(){
         UsersPage usersPage = adminPage.getUsersPage();
         usersPage.getAddNewUserPage();
@@ -112,7 +112,7 @@ public class AddUserTest extends TestBase {
 
     }
 
-    //@Test
+    @Test
     public void _008_create_new_user_with_org_override_CANCEL_message(){
         UsersPage usersPage = adminPage.getUsersPage();
         usersPage.getAddNewUserPage();
@@ -123,7 +123,7 @@ public class AddUserTest extends TestBase {
         addUser.backToUsersPage();
     }
 
-    //@Test
+    @Test
     public void _009_new_user_without_retailer_and_org_id(){
         UsersPage usersPage = adminPage.getUsersPage();
         addUser = usersPage.getAddNewUserPage();
@@ -139,7 +139,7 @@ public class AddUserTest extends TestBase {
 
     }
 
-    //@Test
+    @Test
     public void _010_empty_organization_on_main_page_of_users_after_creation(){
         UsersPage usersPage = adminPage.getUsersPage();
         addUser = usersPage.getAddNewUserPage();
@@ -178,30 +178,27 @@ public class AddUserTest extends TestBase {
 //            }
 
 
-    //@AfterClass
+    @AfterClass
     public void deleteUser(){
 
         boolean res = deleteUser(fillEmailAddress);
         Assert.assertTrue(res, "Failed to delete user");
-
-        wait(WAIT);
+        addUser.backToUsersPage();
 
         boolean res2 = deleteUser(fillEmailAddress2);
-        Assert.assertFalse(res2,"Failed to delete user");
-
-        wait(WAIT);
+        Assert.assertTrue(res2,"Failed to delete user");
+        addUser.backToUsersPage();
 
         boolean res3 = deleteUser(fillEmailAddress3);
-        Assert.assertFalse(res3, "Failed to delete user");
-
-        wait(WAIT);
+        Assert.assertTrue(res3, "Failed to delete user");
+        addUser.backToUsersPage();
 
         boolean res4 = deleteUser(fillEmailAddress4);
-        Assert.assertFalse(res4,"Failed to delete user");
+        Assert.assertTrue(res4,"Failed to delete user");
+        addUser.backToUsersPage();
 
         boolean res5 = deleteUser(fillEmailAddress5);
-        Assert.assertFalse(res5,"Failed to delete user");
-
+        Assert.assertTrue(res5,"Failed to delete user");
         }
     }
 
